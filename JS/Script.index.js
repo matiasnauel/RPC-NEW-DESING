f3cf37f199ac5c327159a58c22b82c7690500ff2
matiasnauel <ayala.matias.ma@gmail.com>
2020-10-02 11:01:51 -0300

    mejorando el diseño de mobile para la pagina, apurando a full

--- JS/Script.index.js
+++ JS/Script.index.js
@@ -33,3 +33,71 @@ function call_mouseout_retroced(id) {
     imagen.style.webkitTransform = "scale(1)";
 }
 
+// Slider transition
+window.addEventListener('load', () => {
+
+
+    // initial slide
+    let slide = 1;
+
+    // total slides
+    let slides = document.querySelectorAll(".slider ul li");
+    total = slides.length;
+
+    // show first side
+    showSlide(1);
+
+    next = document.querySelector(".next");
+    prev = document.querySelector(".prev")
+
+    /**
+     * event next button
+     */
+    next.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        slide++;
+        if (slide > total) { slide = 1; }
+        showSlide(slide);
+    })
+
+    /** 
+     * event prev button
+     */
+    prev.addEventListener("click", (evt) => {
+        evt.preventDefault();
+        slide--;
+        if (slide < 1) { slide = total; }
+        showSlide(slide);
+    })
+
+    /**
+     * show slides
+     * 
+     * @param {number} n 
+     * @return {null}
+     * 
+     */
+    function showSlide(n) {
+        n--; // decrement 1
+        for (i = 0; i < slides.length; i++) {
+            (i == n) ? slides[n].style.display = "block" : slides[i].style.display = "none";
+        }
+    }
+
+})
+
+// Mobile menu desplegar
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("mapita").style.zIndex =  "-1";
+    document.getElementById("redline").style.zIndex =" -1";
+
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mapita").style.zIndex =  "1";
+    document.getElementById("redline").style.zIndex =" 1";
+  }
+
