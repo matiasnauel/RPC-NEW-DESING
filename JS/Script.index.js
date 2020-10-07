f550369b37125e47f5a3ea72fbb9762a03e425e4
matiasnauel <ayala.matias.ma@gmail.com>
2020-10-07 12:18:52 -0300

    ya esta el carrito de la pantalla principal, resolucion  768px

--- JS/Script.index.js
+++ JS/Script.index.js
@@ -116,6 +116,36 @@ function openNavCarrito() {
     document.getElementById("redline").style.zIndex =" 1";
   }
 
-  //Slider productos
-  
+//   Modal iniciar sesión
+if(document.getElementById("btnModal")){
+    var modal = document.getElementById("tvesModal");
+    var btn = document.getElementById("btnModal");
+    var span = document.getElementsByClassName("close")[0];
+    var body = document.getElementsByTagName("body")[0];
 
+    btn.onclick = function() {
+        modal.style.display = "block";
+
+        body.style.position = "static";
+        body.style.height = "100%";
+        body.style.overflow = "hidden";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+
+        body.style.position = "inherit";
+        body.style.height = "auto";
+        body.style.overflow = "visible";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+
+            body.style.position = "inherit";
+            body.style.height = "auto";
+            body.style.overflow = "visible";
+        }
+    }
+}
